7e60aed6d98b68a821db2cfbc3ffd68a13a4959f
Rogério Bayer <tomatebg@gmail.com>
2025-11-17 09:37:39 -0300

    feat(bayerooo): :art: add new font style in all components

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,7 +3,7 @@ import LanguageSelector from "./LanguageSelector";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0 z-50 mx-auto flex w-full max-w-7xl flex-wrap items-center justify-between bg-stone-50/80 backdrop-blur-md p-[2em] font-sans font-bold uppercase text-gray-900 border-b border-gray-100/50">
+    <header className="sticky top-0 z-50 mx-auto flex w-full max-w-7xl flex-wrap items-center justify-between bg-stone-50/80 backdrop-blur-md p-[2em] font-sans font-normal uppercase text-gray-900 border-b border-gray-100/50">
       <Logo />
       <LanguageSelector />
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Stack Sans Text", "sans-serif"],
+        heading: ["Stack Sans Notch", "sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
